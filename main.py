13bef1b343f2502f091fcef868f21da7ada934f7
Corrie <corriehughesy@gmail.com>
2022-06-15 13:28:43 -0700

    Move keys to Heroku environment

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 Base = declarative_base()
@@ -30,7 +31,7 @@ gravatar = Gravatar(app,
                     )
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL1')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
